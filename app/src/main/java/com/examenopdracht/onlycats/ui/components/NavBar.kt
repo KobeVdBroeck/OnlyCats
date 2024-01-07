817951c26a5dee4c7a93b21bec603127564f9067
Kobe Van den Broeck <kobe.vandenbroeck@student.hogent.be>
2024-01-07 16:05:51 +0100

    Added labels to navbar

--- app/src/main/java/com/examenopdracht/onlycats/ui/components/NavBar.kt
+++ app/src/main/java/com/examenopdracht/onlycats/ui/components/NavBar.kt
@@ -6,6 +6,7 @@ import androidx.compose.material.icons.filled.Home
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import com.examenopdracht.onlycats.ui.CatScreen
 
@@ -16,10 +17,12 @@ fun NavBar(
 ) {
         NavigationBar {
             NavigationBarItem(onClick = { navigateTo(CatScreen.Home.name) }, selected = currentScreen == CatScreen.Home,
-                icon = { Icon(Icons.Filled.Home, contentDescription = "NavBar home icon") })
+                icon = { Icon(Icons.Filled.Home, contentDescription = "NavBar home icon") },
+                label = { Text(text = "Home") })
 
             NavigationBarItem(onClick = { navigateTo(CatScreen.Favourites.name) }, selected = currentScreen == CatScreen.Favourites,
-                icon = { Icon(Icons.Filled.Favorite, contentDescription = "NavBar favourites icon") })
+                icon = { Icon(Icons.Filled.Favorite, contentDescription = "NavBar favourites icon") },
+                label = { Text(text = "Favourites") })
         }
 }
 
